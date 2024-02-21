de148f87dd061c53c4853cb66281800a555336d5
0911bekjon <bekjonnorbotayev9@gmail.com>
2024-02-21 21:36:01 +0500

    salom  js2

--- main.js
+++ main.js
@@ -9,39 +9,47 @@
 // key + name = value
 
 
-var simba = 10;
-var timon= "salom";
-var b=timon
+// var simba = 10;
+// var timon= "salom";
+// var b=timon
 
-var pumba=simba + timon
-var kopaytiruv=simba * timon
-var minus=simba - timon
-var foiz=simba / timon
+// var pumba=simba + timon
+// var kopaytiruv=simba * timon
+// var minus=simba - timon
+// var foiz=simba / timon
 
-var c = simba**timon
-var t =simba%timon
+// var c = simba**timon
+// var t =simba%timon
 
-console.log( 
-    // timon == simba
-    // timon === simba
+// console.log( 
+//     // timon == simba
+//     // timon === simba
 
-    typeof(simba), typeof(timon), typeof(true)
-    );
-console.log (
-    pumba, kopaytiruv, minus, foiz
-)
+//     typeof(simba), typeof(timon), typeof(true)
+//     );
+// console.log (
+//     pumba, kopaytiruv, minus, foiz
+// )
 // age=30
 // age=50
 // console.log(pumba)
 // console.log(b);
 
-
-
-
-
-
-
-
 // console.info (age)
 // console.error (age)
 // console.warn (age)
+
+
+
+// a = 15
+
+// var a = 10
+
+// console.log(a)
+
+
+
+let a = 10
+a = 15
+
+console.log(a)
