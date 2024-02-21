887750d5c50ab4e5451502896d6ad79fba76c8b0
0911bekjon <bekjonnorbotayev9@gmail.com>
2024-02-21 21:31:52 +0500

    salom  js

---
+++ main.js
@@ -0,0 +1,47 @@
+// o'zgaruvchilar
+// o'zgaruvchilarni yasash // keys var /let / const
+// o'zgaruvchilarni turlari // types = number / string / bolean = true / false / object undefined / null 
+// console .log .eror .warn .table
+// > < + - / * ** % = ==
+// type off
+
+// 1 punkt
+// key + name = value
+
+
+var simba = 10;
+var timon= "salom";
+var b=timon
+
+var pumba=simba + timon
+var kopaytiruv=simba * timon
+var minus=simba - timon
+var foiz=simba / timon
+
+var c = simba**timon
+var t =simba%timon
+
+console.log( 
+    // timon == simba
+    // timon === simba
+
+    typeof(simba), typeof(timon), typeof(true)
+    );
+console.log (
+    pumba, kopaytiruv, minus, foiz
+)
+// age=30
+// age=50
+// console.log(pumba)
+// console.log(b);
+
+
+
+
+
+
+
+
+// console.info (age)
+// console.error (age)
+// console.warn (age)
